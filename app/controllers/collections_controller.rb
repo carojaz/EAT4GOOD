73c07a24d6b412f366fdb3470364094243fe4a58
Nicholas Claessens <nicholas04claessens@yahoo.com>
2020-12-07 12:06:37 +0100

    collections ok et navbar dynamic

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,9 +1,15 @@
 class CollectionsController < ApplicationController
   def index
-    @collections = Badge.all
+    @badges = Badge.all
+    if Collection.all.count == 0
+      @badges.each do |badge|
+        Collection.create(counter: 0, badge_id: badge.id, user_id: current_user.id)
+      end
+    end
   end
 
   def show
     @badge = Badge.find(params[:id])
+    @collection = Collection.find_by(badge: @badge, user: current_user)
   end
 end
