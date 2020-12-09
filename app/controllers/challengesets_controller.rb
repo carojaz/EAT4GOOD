94b67d7862392722f077f5747a47900151277077
Nicholas Claessens <nicholas04claessens@yahoo.com>
2020-12-09 10:27:45 +0100

    challengeset controller ok

--- app/controllers/challengesets_controller.rb
+++ app/controllers/challengesets_controller.rb
@@ -1,2 +1,10 @@
 class ChallengesetsController < ApplicationController
+  def index
+    @friends = Friend.where(friend1_user: current_user).or(Friend.where(friend2_user: current_user))
+    @challengesets = []
+    @friends.each do |friend|
+      challset = Challengeset.where(friend_id: friend)
+      @challengesets << challset unless challset == []
+    end
+  end
 end
