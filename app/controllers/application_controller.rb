4b068d8f4f3605d70d5229e2ec66150ce3d7f041
caroline jasinski <jasinski_caroline@yahoo.fr>
2020-12-10 12:09:39 +0100

    profile fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-  { host: ENV["DOMAIN"] || "localhost:3000" }
-end
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
